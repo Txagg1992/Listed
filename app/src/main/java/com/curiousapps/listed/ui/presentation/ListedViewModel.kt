5f49708d4dcf5fe8da9481259e305584a6b1145b
Enoch Phetteplace <curiousaps@gmail.com>
2025-02-15 22:54:35 -0600

    Initial ViewModel Setup.

---
+++ app/src/main/java/com/curiousapps/listed/ui/presentation/ListedViewModel.kt
@@ -0,0 +1,53 @@
+package com.curiousapps.listed.ui.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.curiousapps.listed.domain.ListIdItem
+import com.curiousapps.listed.domain.ListIdRepository
+import com.curiousapps.listed.util.IO_DISPATCHER
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ListedViewModel @Inject constructor(
+    private val repository: ListIdRepository
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(ListedState())
+    val state: Flow<ListedState>
+        get() = _state
+
+    init {
+        fetchListIds()
+    }
+
+    private fun fetchListIds(){
+        viewModelScope.launch(IO_DISPATCHER) {
+            val result = repository.fetchListIds()
+            when{
+                result.isSuccess -> {
+                    _state.update { it.copy(
+                        listIdList = result.getOrNull()!!,
+                        isLoading = false
+                    ) }
+                }
+                result.isFailure -> {
+                    _state.update { it.copy(
+                        listIdList = emptyList(),
+                        isLoading = false
+                    ) }
+                }
+            }
+        }
+    }
+
+
+    data class ListedState(
+        val listIdList: List<ListIdItem> = emptyList(),
+        val isLoading: Boolean = true,
+    )
+}
--- app/src/main/java/com/curiousapps/listed/util/Constants.kt
+++ app/src/main/java/com/curiousapps/listed/util/Constants.kt
@@ -1,4 +1,8 @@
 package com.curiousapps.listed.util
 
+import kotlinx.coroutines.Dispatchers
+
 const val BASE_URL = "https://fetch-hiring.s3.amazonaws.com/"
 const val URL_EXT = "hiring.json"
+
+val IO_DISPATCHER = Dispatchers.IO
